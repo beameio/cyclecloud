29034540663752318ae76eda3705e301e8df1eac
Ricardo Gomes <ricardo@heptasoft.com>
2019-08-08 11:21:16 +0100

    Added default for test simplicity. Since linux doesn't accept multiple customscripts, joined all in one install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+applicationSecret=$1
+applicationId=$2
+tenantId=$3
+azureSovereignCloud=$4
+cycleDownloadURL=$5
+cyclecloudVersion=$6
+username=$7
+cycleFqdn=$8
+storageAccountLocation=$9
+
+echo "Installing CycleCloud"
+python cyclecloud_install.py --applicationSecret $applicationSecret --applicationId $applicationId --tenantId $tenantId --azureSovereignCloud $azureSovereignCloud --downloadURL $cycleDownloadURL --cyclecloudVersion $cyclecloudVersion --username $username --hostname $cycleFqdn --storageAccount $storageAccountLocation
+
+echo "Installing htcondor template"
+cyclecloud project fetch https://github.com/beameio/cyclecloud-htcondor /opt/cycle_server/work/staging/projects/custom-htcondor 
+cp /opt/cycle_server/work/staging/projects/htcondor/blobs /opt/cycle_server/work/staging/projects/custom-htcondor/ -rv 
+(cd /opt/cycle_server/work/staging/projects/custom-htcondor && cyclecloud project build && cyclecloud import_template -f templates/htcondor.txt)
