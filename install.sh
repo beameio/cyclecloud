764dc15fae82dc5181e79bfa3b84299c8c069417
Ricardo Gomes <ricardo@heptasoft.com>
2019-08-09 12:45:52 +0100

    Added password to template and ssh key handling to the script

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ cyclecloudVersion=$6
 username=$7
 cycleFqdn=$8
 storageAccountLocation=$9
+password=${10}
+sshKey=${11}
 
 echo "Installing CycleCloud"
 python cyclecloud_install.py --applicationSecret "$applicationSecret" --applicationId "$applicationId" --tenantId "$tenantId" --azureSovereignCloud "$azureSovereignCloud" --downloadURL "$cycleDownloadURL" --cyclecloudVersion "$cyclecloudVersion" --username "$username" --hostname "$cycleFqdn" --storageAccount "$storageAccountLocation"
 
+##### -- misses initialization of the cluster with username, password, sshKey, then next can be uncommented
+
 echo "Installing htcondor template"
 /usr/local/bin/cyclecloud project fetch https://github.com/beameio/cyclecloud-htcondor /root/cyclecloud-htcondor 
-
 # /usr/local/bin/cyclecloud import_template -f /root/cyclecloud-htcondor/templates/htcondor.txt
-
-# cp /opt/cycle_server/work/staging/projects/htcondor/blobs /opt/cycle_server/work/staging/projects/custom-htcondor/ -rv
-# (cd /opt/cycle_server/work/staging/projects/custom-htcondor && cyclecloud project build && cyclecloud import_template -f templates/htcondor.txt)
