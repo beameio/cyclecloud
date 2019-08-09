3f933c0d140cc77c409fac1ee62e629ab1367588
Ricardo Gomes <ricardo@heptasoft.com>
2019-08-09 11:23:43 +0100

    Try on the fetch

--- install.sh
+++ install.sh
@@ -14,8 +14,9 @@ echo "Installing CycleCloud"
 python cyclecloud_install.py --applicationSecret "$applicationSecret" --applicationId "$applicationId" --tenantId "$tenantId" --azureSovereignCloud "$azureSovereignCloud" --downloadURL "$cycleDownloadURL" --cyclecloudVersion "$cyclecloudVersion" --username "$username" --hostname "$cycleFqdn" --storageAccount "$storageAccountLocation"
 
 echo "Installing htcondor template"
-/usr/local/bin/cyclecloud project fetch https://github.com/beameio/cyclecloud-htcondor /root/custom-htcondor
-/usr/local/bin/cyclecloud import_template -f /root/custom-htcondor/templates/htcondor.txt
+/usr/local/bin/cyclecloud project fetch https://github.com/beameio/cyclecloud-htcondor /root/cyclecloud-htcondor 
+
+# /usr/local/bin/cyclecloud import_template -f /root/cyclecloud-htcondor/templates/htcondor.txt
 
 # cp /opt/cycle_server/work/staging/projects/htcondor/blobs /opt/cycle_server/work/staging/projects/custom-htcondor/ -rv
 # (cd /opt/cycle_server/work/staging/projects/custom-htcondor && cyclecloud project build && cyclecloud import_template -f templates/htcondor.txt)
