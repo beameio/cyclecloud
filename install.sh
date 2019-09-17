181bec5fc65090446bdf4a75fc7e2a9b4e4337f1
Ricardo Gomes <ricardo@heptasoft.com>
2019-09-17 12:55:29 +0100

    Improved Readme Added htcondor import.

--- install.sh
+++ install.sh
@@ -15,9 +15,8 @@ sshKey=${11}
 echo "Installing CycleCloud"
 python cyclecloud_install.py --cyclecloudVersion "$cyclecloudVersion" --downloadURL "$cycleDownloadURL" --azureSovereignCloud "$azureSovereignCloud" --tenantId "$tenantId" --applicationId "$applicationId" --applicationSecret "$applicationSecret" --username "$username" --hostname "$cycleFqdn" --acceptTerms --password "${password}" --storageAccount "$storageAccountLocation"
 
-##### -- misses initialization of the cluster with username, password, sshKey
-
-echo "Installing htcondor template"
+echo "Fetching htcondor template"
 /usr/local/bin/cyclecloud project fetch https://github.com/beameio/cyclecloud-htcondor /root/cyclecloud-htcondor 
-#### next can be uncommented after cluster initialization
-# /usr/local/bin/cyclecloud import_template -f /root/cyclecloud-htcondor/templates/htcondor.txt
+
+echo "Importing htcondor template"
+/usr/local/bin/cyclecloud import_template -f /root/cyclecloud-htcondor/templates/htcondor.txt
